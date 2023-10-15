35122a9e1ddf61a966bca8a2d811b296ceb40a36
anujkp-97 <anujpal9770@gmail.com>
2023-10-15 18:17:03 +0530

    commit

--- src/recursion.java
+++ src/recursion.java
@@ -2,7 +2,7 @@ public class recursion {
     public static void main(String[] args) {
         
         System.out.println("Recursion is used to call thee method again and again.");
-        System.out.println("this is done");
+       
     }
     
 }
