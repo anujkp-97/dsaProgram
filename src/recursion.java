45624d6a02555d8d7409afc72acfef5efddd387c
anujkp-97 <anujpal9770@gmail.com>
2023-10-15 18:43:08 +0530

    apna

--- src/recursion.java
+++ src/recursion.java
@@ -1,13 +1,11 @@
 public class recursion {
     public static void main(String[] args) {
         
-        System.out.println("Recursion is used to call thee method again and again.");
-
-        
+        System.out.println("Recursion is used to call thee method again and again."); 
        
       System.out.println("This line is from github");
-
-    
+      
+      System.out.println("now let's talk abot github");
         
 
     }
